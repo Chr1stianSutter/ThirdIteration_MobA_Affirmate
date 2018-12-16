a5a6fade963be08d2ad13d99b239a9b4a0d58b8c
Chris <sutter.christian95@gmail.com>
2018-12-16 11:27:34 +0100

    linked cancel and save button back to main activity
    save button intent doesnt carry data yet

--- app/src/main/java/com/example/christian/thirditeration_moba_affirmate/NewCardActivity.java
+++ app/src/main/java/com/example/christian/thirditeration_moba_affirmate/NewCardActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class NewCardActivity extends AppCompatActivity {
 
@@ -24,6 +26,24 @@ public class NewCardActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // back button pressed
+                Intent myIntent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(myIntent);
+            }
+        });
+
+        Button cancelButton = (Button) findViewById(R.id.cancelButtonPressed);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(myIntent);
+            }
+        });
+
+        Button saveButton = (Button) findViewById(R.id.saveButtonPressed);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 Intent myIntent = new Intent(getBaseContext(), MainActivity.class);
                 startActivity(myIntent);
             }
