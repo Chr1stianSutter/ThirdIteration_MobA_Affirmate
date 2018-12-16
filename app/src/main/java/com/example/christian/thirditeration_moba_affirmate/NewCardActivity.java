ad50f1ee657b2eb96d5892a81efad2ad11647a74
Chris <sutter.christian95@gmail.com>
2018-12-16 13:24:11 +0100

    Added Time Picker
    Cant Save new time yet

--- app/src/main/java/com/example/christian/thirditeration_moba_affirmate/NewCardActivity.java
+++ app/src/main/java/com/example/christian/thirditeration_moba_affirmate/NewCardActivity.java
@@ -1,15 +1,25 @@
 package com.example.christian.thirditeration_moba_affirmate;
 
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.app.Dialog;
+import android.text.format.DateFormat;
+import android.support.v4.app.DialogFragment;
+import java.util.Calendar;
+
 
 public class NewCardActivity extends AppCompatActivity {
 
+    TextView tvDisplayTime;
+    Integer hour;
+    Integer minute;
 
 
     @Override
@@ -31,6 +41,8 @@ public class NewCardActivity extends AppCompatActivity {
             }
         });
 
+
+
         Button cancelButton = (Button) findViewById(R.id.cancelButtonPressed);
         cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,5 +61,31 @@ public class NewCardActivity extends AppCompatActivity {
             }
         });
     }
+/*
+    public void setCurrentTimeOnView(){
+        tvDisplayTime = (TextView) findViewById(R.id.tvTime);
+
+        final Calendar c = Calendar.getInstance();
+        hour = c.get(Calendar.HOUR_OF_DAY);
+        minute = c.get(Calendar.MINUTE);
+        tvDisplayTime.setText(new StringBuilder().append(hour)
+        .append(":").append(minute));
+    }
+*/
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    private TimePickerDialog.OnTimeSetListener timePickerListener =
+            new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker view, int selectedHour, int selectedMinute) {
+                    hour = selectedHour;
+                    minute = selectedMinute;
+
+                    tvDisplayTime.setText(new StringBuilder().append(hour).append(":").append(minute));
+                }
+            };
 
 }
