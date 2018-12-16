ba7c8fe5244f26cf26602b52c7de14198331a0aa
Chris <sutter.christian95@gmail.com>
2018-12-16 18:14:11 +0100

    added Am PM to time picker, moved "edit"-icon cloaer to textview

--- app/src/main/java/com/example/christian/thirditeration_moba_affirmate/TimePickerFragment.java
+++ app/src/main/java/com/example/christian/thirditeration_moba_affirmate/TimePickerFragment.java
@@ -4,13 +4,13 @@ package com.example.christian.thirditeration_moba_affirmate;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.DialogFragment;
 import android.text.format.DateFormat;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import java.util.Calendar;
-
-
+import java.lang.*;
 
 
 public class TimePickerFragment extends DialogFragment
@@ -20,6 +20,10 @@ public class TimePickerFragment extends DialogFragment
     Integer minute;
 
 
+    private static String  pad(int c)
+    {
+        return c>=10 ? ""+c : "0"+c;
+    }
 
 
     @Override
@@ -29,10 +33,29 @@ public class TimePickerFragment extends DialogFragment
         int hour = c.get(Calendar.HOUR_OF_DAY);
         int minute = c.get(Calendar.MINUTE);
 
+
+
+/*
+        //Take Hour:Minute from last input value (8:30)
+        TextView tvDisplayTime = (TextView) getActivity().findViewById(R.id.tvTime);
+
+        String tvDisplayTimeString = tvDisplayTime.toString();
+
+        String substr = ":";
+        String before = tvDisplayTimeString.substring(0, tvDisplayTimeString.indexOf(substr));
+        String after = tvDisplayTimeString.substring(tvDisplayTimeString.indexOf(substr) + substr.length());
+
+        int hour = Integer.valueOf(before);
+        int minute = Integer.valueOf(after);
+
+*/
+
         return new TimePickerDialog(getActivity(), this, hour, minute,
                 DateFormat.is24HourFormat(getActivity()));
-    }
 
+
+
+    }
 
 
 
@@ -43,7 +66,16 @@ public class TimePickerFragment extends DialogFragment
         hour = selectedHour;
         minute = selectedMinute;
 
-        tvDisplayTime.setText(new StringBuilder().append(hour).append(":").append(minute));
+        String AM_PM;
+        if (selectedHour >=0 && selectedHour < 12){
+            AM_PM = " AM";
+        } else {
+            AM_PM = " PM";
+        }
+
+        tvDisplayTime.setText(new StringBuilder().append(hour).append(":").append(pad(minute)).append(AM_PM));
+
+
 
     }
 
